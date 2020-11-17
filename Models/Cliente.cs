1fb7aa977100ac2f3af955bb3431bbef3cf7e50d
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-11-17 10:40:40 -0300

    versionando alteração com turma 11b itaú

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -5,7 +5,7 @@ namespace WebMVCRazor.Models
 {
     public class Cliente
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
         public string Nome { get; set; }
         public string Telefone { get; set; }
 
